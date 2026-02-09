e1842341631e78e06d075ab3e6dc20fd7fb2a376
Leafyc <yuanchenliu05@Gmail.com>
2026-02-09 16:09:21 -0500

    New project

---
+++ Celeste/Celeste/Madeline/standMadeline.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Celeste.Madeline
+{
+    public class standMadeline : IMadeline
+    {
+
+        public void update(Game1 game)
+        {
+           
+        }
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+    }
+}
